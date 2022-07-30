643d8b54de85245306ec4ccaff744bc7fc6428ea
zhenyuwei99 <zhenyuwei99@gmail.com>
2022-07-30 23:07:20 +0800

    Update main.py for caluclation I-V curve

--- code/pnp_solver/main.py
+++ code/pnp_solver/main.py
@@ -14,26 +14,21 @@ import os
 import mdpy as md
 import matplotlib.pyplot as plt
 import cupy as cp
+from mdpy.core import Grid
+from mdpy.io import GridWriter
 from mdpy.unit import *
-from grid import Grid
+from mdpy.utils import *
+from mdpy.environment import *
 from fd_pnp_constraint import FDPoissonNernstPlanckConstraint
 
-if __name__ == "__main__":
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    str_dir = os.path.join(cur_dir, "str")
-    os.system("rm -rf %s/*.png" % cur_dir)
-    # Hyper parameter
-    voltage = (
-        Quantity(0.1, volt).convert_to(default_energy_unit / default_charge_unit).value
-    )
+
+def job(voltage, sod_density, cla_density):
+    voltage = check_quantity_value(voltage, default_energy_unit / default_charge_unit)
+    sod_density = check_quantity_value(sod_density, 1 / default_length_unit ** 3)
+    cla_density = check_quantity_value(cla_density, 1 / default_length_unit ** 3)
     sod_diffusion_coefficient = (
         Quantity(1.334 * 1e-9, meter ** 2 / second)
         .convert_to(default_length_unit ** 2 / default_time_unit)
-        .value
-    )
-    sod_density = (
-        (Quantity(1.0, mol / decimeter ** 3) * NA)
-        .convert_to(1 / default_length_unit ** 3)
         .value
     )
     cla_diffusion_coefficient = (
@@ -41,32 +36,44 @@ if __name__ == "__main__":
         .convert_to(default_length_unit ** 2 / default_time_unit)
         .value
     )
-    cla_density = sod_density
     # Read structure
     pdb = md.io.PDBParser(os.path.join(str_dir, "sio2_pore.pdb"))
     psf = md.io.PSFParser(os.path.join(str_dir, "sio2_pore.psf"))
     topology = psf.topology
+    topology.split()
+    topology.add_particles(
+        [
+            md.core.Particle(charge=1, molecule_type="FIX"),
+            md.core.Particle(charge=1, molecule_type="FIX"),
+        ]
+    )
+    radius = 5
+    extend_radius = radius * 1.1
+    positions = np.zeros([topology.num_particles, 3], NUMPY_FLOAT)
+    positions[: pdb.positions.shape[0], :] = pdb.positions
+    positions[-2, :] = [-radius, 0, 0]
+    positions[-1, :] = [radius, 0, 0]
     # for particle in topology.particles:
     #   particle._charge = 0
     # topology.join()
     pbc = pdb.pbc_matrix
-    pbc[2, 2] *= 4
+    pbc[2, 2] *= 8
     ensemble = md.core.Ensemble(topology, pbc, is_use_tile_list=False)
-    ensemble.state.set_positions(pdb.positions)
+    ensemble.state.set_positions(positions)
     # Solver
     grid = Grid(
         x=[-pbc[0, 0] / 2, pbc[0, 0] / 2, 128],
         y=[-pbc[1, 1] / 2, pbc[1, 1] / 2, 128],
         z=[-pbc[2, 2] / 2, pbc[2, 2] / 2, 256],
     )
-    # Set constraint
     constraint = FDPoissonNernstPlanckConstraint(
         Quantity(300, kelvin), grid, sod=1, cla=-1
     )
-    r = cp.sqrt(grid.x ** 2 + grid.y ** 2)
+    r = cp.sqrt(grid.coordinate.x ** 2 + grid.coordinate.y ** 2)
     alpha = 2
     nanopore_shape = 1 / (
-        (1 + cp.exp(-alpha * (r - 9.5))) * (1 + cp.exp(alpha * (cp.abs(grid.z) - 20)))
+        (1 + cp.exp(-alpha * (r - extend_radius)))
+        * (1 + cp.exp(alpha * (cp.abs(grid.coordinate.z) - 18)))
     )  # 1 for pore 0 for solvation
     channel_shape = nanopore_shape >= 0.5
     constraint.grid.add_field("channel_shape", channel_shape)
@@ -81,43 +88,49 @@ if __name__ == "__main__":
     # sod
     sod_diffusion_coefficient = (1 - nanopore_shape) * sod_diffusion_coefficient
     constraint.grid.add_field("sod_diffusion_coefficient", sod_diffusion_coefficient)
-    sod_density = (1 - nanopore_shape) * sod_density
+    tmp = sod_density
+    sod_density = (1 - nanopore_shape) * tmp
     sod_density[[0, -1], :, :] = 0
     sod_density[:, [0, -1], :] = 0
-    sod_density[:, :, [0, -1]] = 0
+    sod_density[:, :, [0, -1]] = tmp
     constraint.grid.add_field("sod_density", sod_density)
     # cla
     cla_diffusion_coefficient = (1 - nanopore_shape) * cla_diffusion_coefficient
     constraint.grid.add_field("cla_diffusion_coefficient", cla_diffusion_coefficient)
+    tmp = cla_density
     cla_density = (1 - nanopore_shape) * cla_density
     cla_density[[0, -1], :, :] = 0
     cla_density[:, [0, -1], :] = 0
-    cla_density[:, :, [0, -1]] = 0
+    cla_density[:, :, [0, -1]] = tmp
     constraint.grid.add_field("cla_density", cla_density)
 
     ensemble.add_constraints(constraint)
     ensemble.update_constraints()
 
-    grid = 64
-    fig, ax = plt.subplots(1, 1, figsize=[16, 9])
-    fig.tight_layout()
-    # c = ax.contourf(
-    #     constraint.grid.x[1:-1, grid, 1:-1].get(),
-    #     constraint.grid.z[1:-1, grid, 1:-1].get(),
-    #     constraint.grid.field.exclusion_potential_energy[1:-1, grid, 1:-1].get(),
-    #     100,
-    # )
-    c = ax.contourf(
-        constraint.grid.x[1:-1, grid, 1:-1].get(),
-        constraint.grid.z[1:-1, grid, 1:-1].get(),
-        constraint.grid.field.electric_potential[1:-1, grid, 1:-1].get(),
-        100,
+    voltage = (
+        Quantity(voltage, default_energy_unit / default_charge_unit)
+        .convert_to(volt)
+        .value
     )
-    # c = ax.contourf(
-    #     constraint.grid.x[1:-1, 1:-1, grid].get(),
-    #     constraint.grid.y[1:-1, 1:-1, grid].get(),
-    #     constraint.grid.field.electric_potential[1:-1, 1:-1, grid].get(),
-    #     100,
-    # )
-    plt.colorbar(c)
-    plt.savefig("res.png")
+    file_name = (
+        "pnp-minus-%.3f-volt.grid" if voltage <= 0 else "pnp-plus-%.3f-volt.grid"
+    )
+    writer = GridWriter(os.path.join(out_dir, file_name % abs(voltage)))
+    writer.write(constraint.grid)
+
+
+if __name__ == "__main__":
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    str_dir = os.path.join(cur_dir, "str")
+    out_dir = os.path.join(cur_dir, "out/5nm-pre-fixed-2")
+    os.system("rm -rf %s/*.png" % cur_dir)
+    # Hyper parameter
+    for v in np.linspace(-2, 2, 20, endpoint=True):
+        voltage = (
+            Quantity(v, volt)
+            .convert_to(default_energy_unit / default_charge_unit)
+            .value
+        )
+        sod_density = Quantity(0.5, mol / decimeter ** 3) * NA
+        cla_density = sod_density
+        job(voltage=voltage, sod_density=sod_density, cla_density=cla_density)
