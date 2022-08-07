f785b543e2a59a2b218d2ef3cc5a68ba008091f9
zhenyuwei99 <zhenyuwei99@gmail.com>
2022-08-07 22:47:29 +0800

    Update analyzer

--- code/pnp_solver/analyzer.py
+++ code/pnp_solver/analyzer.py
@@ -12,7 +12,9 @@ copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 import os
 import numpy as np
 import mdpy as md
+import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
 from mdpy.unit import *
 from mdpy.utils import check_quantity
 
@@ -55,38 +57,69 @@ def analysis(
 
 if __name__ == "__main__":
     cur_dir = os.path.dirname(os.path.abspath(__file__))
-    out_dir = os.path.join(cur_dir, "out/15nm-pore-no-fixed/")
-    target_files = [os.path.join(out_dir, i) for i in os.listdir(out_dir)]
+    out_dir = os.path.join(cur_dir, "out/5a-pore-250a-thick")
+    file_name = out_dir.split("/")[-1]
+    target_files = [
+        os.path.join(out_dir, i, "res.grid")
+        for i in os.listdir(out_dir)
+        if os.path.exists(os.path.join(out_dir, i, "res.grid"))
+    ]
     ele_list = []
-    sod_res, cla_res = [], []
+    pot_res, cla_res = [], []
     for target_file in target_files:
         factor = -1 if "minus" in target_file else 1
-        ele_list.append(factor * float(target_file.split("/")[-1].split("-")[2]))
+        ele_list.append(factor * float(target_file.split("/")[-2].split("-")[2]))
     ele_list, target_files = [
         list(i) for i in zip(*sorted(zip(ele_list, target_files)))
     ]
     for target_file in target_files:
-        index = 128
+        index = 256
         grid = md.io.GridParser(target_file).grid
-        index = grid.inner_shape[1] // 2
-        sod_res.append(analysis(grid, "sod", 1, index))
+        pot_res.append(analysis(grid, "pot", 1, index))
         cla_res.append(analysis(grid, "cla", -1, index))
         print(target_file)
-    sod_res = np.array(sod_res)
+    pot_res = np.array(pot_res)
     cla_res = np.array(cla_res)
-    fig, ax = plt.subplots(1, 1, figsize=[16, 16])
-    print(sod_res)
+    fig, ax = plt.subplots(1, 1, figsize=[20, 8])
+    font_big, font_mid = 20, 15
+    #     ax.plot(
+    #         ele_list,
+    #         Quantity(pot_res, default_charge_unit / default_time_unit)
+    #         .convert_to(ampere)
+    #         .value,
+    #         ".-",
+    #         label="Pot solution",
+    #     )
+    #     ax.plot(
+    #         ele_list,
+    #         Quantity(cla_res, default_charge_unit / default_time_unit)
+    #         .convert_to(ampere)
+    #         .value,
+    #         ".-",
+    #         label="Cla solution",
+    #     )
     ax.plot(
         ele_list,
-        Quantity(sod_res, default_charge_unit / default_time_unit)
+        Quantity(pot_res + cla_res, default_charge_unit / default_time_unit)
         .convert_to(ampere)
         .value,
+        ".-",
+        label="PNP solution",
+        color="navy",
     )
-    ax.plot(
-        ele_list,
-        Quantity(cla_res, default_charge_unit / default_time_unit)
-        .convert_to(ampere)
-        .value,
-    )
-    plt.savefig("pnp.png")
+    if True:
+        experiment_file = os.path.join(cur_dir, "experiment/1nm-1mol.xlsx")
+        df = pd.read_excel(experiment_file, header=None)
+        ele_list = np.array(df[0]) / 1000
+        current_list = np.array(df[1])
+        if False:
+            ele_list *= -1
+            current_list *= -1
+        ax.plot(ele_list, current_list, ".-", label="Experiment", color="brown")
+    ax.set_xlabel("Voltage (V)", fontsize=font_big)
+    ax.set_ylabel("Current (A)", fontsize=font_big)
+    ax.tick_params(labelsize=font_mid)
+    ax.legend(fontsize=font_big)
+    fig.tight_layout()
+    plt.savefig(os.path.join(cur_dir, file_name + ".png"))
     plt.close()
