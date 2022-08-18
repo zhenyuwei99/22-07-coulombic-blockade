a92eaaaf303db9731a5e354e85caf4567c09d7f4
zhenyuwei99 <zhenyuwei99@gmail.com>
2022-08-18 12:28:18 +0800

    Update parameterization

--- code/pnp_solver/parametrization.py
+++ code/pnp_solver/parametrization.py
@@ -9,6 +9,7 @@ contact : zhenyuwei99@gmail.com
 copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 """
 
+import traceback
 import os
 import mdpy as md
 import cupy as cp
@@ -18,6 +19,7 @@ from itertools import product
 from mdpy.utils import check_quantity_value
 from mdpy.unit import *
 from mdpy.environment import *
+from nelder_mead import NelderMeadMinimizer
 from main import check_dir
 from analyzer import analysis
 from fd_pnp_constraint import *
@@ -137,98 +139,117 @@ def job(
     str_dir: str,
     root_dir: str,
 ):
-    job_name = generate_job_name(ls_pot=ls_pot, ls_cla=ls_cla, voltage=voltage)
-    out_dir = check_dir(os.path.join(root_dir, job_name))
-    log_file = os.path.join(out_dir, "log.txt")
-    grid_file = os.path.join(out_dir, "res.grid")
-    if not os.path.exists(grid_file):
-        device, job = get_available_device(device_file_path)
-        with open(log_file, "w") as f:
-            print("Submit %s to device-%d-job-%d" % (job_name, device, job), file=f)
-        with md.device.Device(device):
+    try:
+        job_name = generate_job_name(ls_pot=ls_pot, ls_cla=ls_cla, voltage=voltage)
+        out_dir = check_dir(os.path.join(root_dir, job_name))
+        log_file = os.path.join(out_dir, "log.txt")
+        grid_file = os.path.join(out_dir, "res.grid")
+        if not os.path.exists(grid_file):
+            device, job = get_available_device(device_file_path)
+            with open(log_file, "w") as f:
+                print("Submit %s to device-%d-job-%d" % (job_name, device, job), file=f)
             register_device(device_file_path, device, job)
-            # Create ensemble
-            pdb = md.io.PDBParser(os.path.join(str_dir, "sio2_pore.pdb"))
-            psf = md.io.PSFParser(os.path.join(str_dir, "sio2_pore.psf"))
-            grid_matrix = pdb.pbc_matrix
-            grid_matrix[2, 2] += 40 * 2
-            topology = psf.topology
-            positions = pdb.positions
-            ensemble = md.core.Ensemble(topology, grid_matrix, is_use_tile_list=False)
-            ensemble.state.set_positions(positions)
-            # Generate grid
-            r0 = Quantity(20, angstrom)
-            l = Quantity(pdb.pbc_matrix[2, 2], angstrom)
-            grid = md.core.Grid(
-                x=[-grid_matrix[0, 0] / 2, grid_matrix[0, 0] / 2, 128],
-                y=[-grid_matrix[1, 1] / 2, grid_matrix[1, 1] / 2, 128],
-                z=[-grid_matrix[2, 2] / 2, grid_matrix[2, 2] / 2, 256],
-            )
-            grid.add_field(
-                "channel_shape",
-                generate_channel_shape(grid=grid, r0=r0, l=l, lb=Quantity(1, angstrom)),
-            )
-            grid.add_field(
-                "relative_permittivity",
-                generate_relative_permittivity_field(
-                    grid=grid,
-                    r0=r0,
-                    l=l,
-                    lb=Quantity(1, angstrom),
-                    ls=Quantity(1, angstrom),
-                ),
-            )
-            grid.add_field(
-                "electric_potential",
-                generate_electric_potential_field(grid=grid, voltage=voltage),
-            )
-            # Pot
-            pot_diffusion = Quantity(1.96 * 1e-9, meter ** 2 / second)
-            grid.add_field(
-                "pot_diffusion_coefficient",
-                generate_diffusion_field(
-                    grid=grid, r0=r0, l=l, ls=ls_pot, diffusion=pot_diffusion, A=0.1,
-                ),
-            )
-            grid.add_field(
-                "pot_density",
-                generate_density_field(
-                    grid=grid, density=Quantity(1.0, mol / decimeter ** 3) * NA
-                ),
-            )
-            # Cla
-            cla_diffusion = Quantity(2.032 * 1e-9, meter ** 2 / second)
-            grid.add_field(
-                "cla_diffusion_coefficient",
-                generate_diffusion_field(
-                    grid=grid, r0=r0, l=l, ls=ls_cla, diffusion=cla_diffusion, A=0.1,
-                ),
-            )
-            grid.add_field(
-                "cla_density",
-                generate_density_field(
-                    grid=grid, density=Quantity(1.0, mol / decimeter ** 3) * NA
-                ),
-            )
-            # Create constraint
-            constraint = FDPoissonNernstPlanckConstraint(
-                Quantity(300, kelvin), grid, pot=1, cla=-1
-            )
-            constraint.set_log_file(log_file, "a")
-            constraint.set_img_dir(out_dir)
-            ensemble.add_constraints(constraint)
-            constraint.update(
-                max_iterations=1500, error_tolerance=1e-3, image_dir=out_dir
-            )
+            with md.device.Device(device):
+                register_device(device_file_path, device, job)
+                # Create ensemble
+                pdb = md.io.PDBParser(os.path.join(str_dir, "sio2_pore.pdb"))
+                psf = md.io.PSFParser(os.path.join(str_dir, "sio2_pore.psf"))
+                grid_matrix = pdb.pbc_matrix
+                grid_matrix[2, 2] += 40 * 2
+                topology = psf.topology
+                positions = pdb.positions
+                ensemble = md.core.Ensemble(
+                    topology, grid_matrix, is_use_tile_list=False
+                )
+                ensemble.state.set_positions(positions)
+                # Generate grid
+                r0 = Quantity(20, angstrom)
+                l = Quantity(pdb.pbc_matrix[2, 2], angstrom)
+                grid = md.core.Grid(
+                    x=[-grid_matrix[0, 0] / 2, grid_matrix[0, 0] / 2, 128],
+                    y=[-grid_matrix[1, 1] / 2, grid_matrix[1, 1] / 2, 128],
+                    z=[-grid_matrix[2, 2] / 2, grid_matrix[2, 2] / 2, 256],
+                )
+                grid.add_field(
+                    "channel_shape",
+                    generate_channel_shape(
+                        grid=grid, r0=r0, l=l, lb=Quantity(1, angstrom)
+                    ),
+                )
+                grid.add_field(
+                    "relative_permittivity",
+                    generate_relative_permittivity_field(
+                        grid=grid,
+                        r0=r0,
+                        l=l,
+                        lb=Quantity(1, angstrom),
+                        ls=Quantity(1, angstrom),
+                    ),
+                )
+                grid.add_field(
+                    "electric_potential",
+                    generate_electric_potential_field(grid=grid, voltage=voltage),
+                )
+                # Pot
+                pot_diffusion = Quantity(1.96 * 1e-9, meter ** 2 / second)
+                grid.add_field(
+                    "pot_diffusion_coefficient",
+                    generate_diffusion_field(
+                        grid=grid,
+                        r0=r0,
+                        l=l,
+                        ls=ls_pot,
+                        diffusion=pot_diffusion,
+                        A=0.1,
+                    ),
+                )
+                grid.add_field(
+                    "pot_density",
+                    generate_density_field(
+                        grid=grid, density=Quantity(1.0, mol / decimeter ** 3) * NA
+                    ),
+                )
+                # Cla
+                cla_diffusion = Quantity(2.032 * 1e-9, meter ** 2 / second)
+                grid.add_field(
+                    "cla_diffusion_coefficient",
+                    generate_diffusion_field(
+                        grid=grid,
+                        r0=r0,
+                        l=l,
+                        ls=ls_cla,
+                        diffusion=cla_diffusion,
+                        A=0.1,
+                    ),
+                )
+                grid.add_field(
+                    "cla_density",
+                    generate_density_field(
+                        grid=grid, density=Quantity(1.0, mol / decimeter ** 3) * NA
+                    ),
+                )
+                # Create constraint
+                constraint = FDPoissonNernstPlanckConstraint(
+                    Quantity(300, kelvin), grid, pot=1, cla=-1
+                )
+                constraint.set_log_file(log_file, "a")
+                constraint.set_img_dir(out_dir)
+                ensemble.add_constraints(constraint)
+                constraint.update(
+                    max_iterations=1500, error_tolerance=1e-3, image_dir=out_dir
+                )
 
-            writer = md.io.GridWriter(grid_file)
-            writer.write(constraint.grid)
-        free_device(device_file_path, device, job)
-    else:
-        print("Job %s exists, skipping current job" % job_name)
+                writer = md.io.GridWriter(grid_file)
+                writer.write(constraint.grid)
+            free_device(device_file_path, device, job)
+        else:
+            print("Job %s exists, skipping current job" % job_name)
+    except:
+        error = traceback.format_exc()
+        raise Exception(error)
 
 
-VOLTAGE_LIST = [-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0]
+VOLTAGE_LIST = [-1, -0.8, -0.6, -0.4, -0.2, 0.2, 0.4, 0.6, 0.8, 1.0]
 REFERENCE_CURRENT = np.array(
     [
         -5.08e-9,
@@ -236,7 +257,6 @@ REFERENCE_CURRENT = np.array(
         -2.97e-9,
         -2.29e-9,
         -1.28e-9,
-        -4.39e-11,
         9.8e-10,
         1.7e-9,
         2.5e-9,
@@ -266,39 +286,16 @@ def calculate_error(root_dir, ls_pot, ls_cla):
         .value
     )
     error = np.abs((pot_res + cla_res - REFERENCE_CURRENT) / REFERENCE_CURRENT).mean()
-    print(job_name, error)
+    return error
 
 
 if __name__ == "__main__":
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     str_dir = os.path.join(cur_dir, "str")
-    root_dir = check_dir(os.path.join(cur_dir, "out/parametrization"))
-    ls_pot_list = [15, 20, 25, 30]
-    ls_cla_list = [15, 20, 25, 30]
-    if True:
-        grid_matrix = np.eye(3)
-        grid_matrix[0] = 100
-        grid_matrix[1] = 100
-        grid_matrix[2] = 350
-        grid = md.core.Grid(
-            x=[-grid_matrix[0, 0] / 2, grid_matrix[0, 0] / 2, 128],
-            y=[-grid_matrix[1, 1] / 2, grid_matrix[1, 1] / 2, 128],
-            z=[-grid_matrix[2, 2] / 2, grid_matrix[2, 2] / 2, 256],
-        )
-        diffusion = generate_diffusion_field(
-            grid=grid, r0=20, l=250, ls=5, diffusion=1, A=0.1
-        )
-        visulize_field(grid, diffusion)
-    if not True:
-        ls_pot_list = [15, 20]
-        ls_cla_list = [15, 20, 25, 30]
-        for ls_pot, ls_cla in product(ls_pot_list, ls_cla_list):
-            calculate_error(root_dir, ls_pot, ls_cla)
-    if not True:
-        voltage_list = [-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0]
-        job_list = list(product(ls_pot_list, ls_cla_list, voltage_list))
-        print("%d jobs in total" % len(job_list))
-        # Manager
+    root_dir = check_dir(os.path.join(cur_dir, "out/nelder_mead"))
+
+    def objective_fun(ls_pot, ls_cla):
+        voltage_list = [-1, -0.8, -0.6, -0.4, -0.2, 0.2, 0.4, 0.6, 0.8, 1.0]  # Manager
         num_devices = 3
         num_jobs_per_device = 2
         num_total_jobs = num_devices * num_jobs_per_device
@@ -308,7 +305,7 @@ if __name__ == "__main__":
             num_jobs_per_device=num_jobs_per_device,
         )
         pool = mp.Pool(num_total_jobs)
-        for ls_pot, ls_cla, voltage in job_list:
+        for voltage in voltage_list:
             ls_pot = Quantity(ls_pot, angstrom)
             ls_cla = Quantity(ls_cla, angstrom)
             voltage = Quantity(voltage, volt)
@@ -321,3 +318,17 @@ if __name__ == "__main__":
             sleep(0.5)
         pool.close()
         pool.join()
+        error = calculate_error(
+            root_dir,
+            check_quantity_value(ls_pot, default_length_unit),
+            check_quantity_value(ls_cla, default_length_unit),
+        )
+        return error
+
+    res_file = os.path.join(root_dir, "minimizer.npz")
+    minimizer = NelderMeadMinimizer(
+        objective_fun=objective_fun, num_dimensions=2, result_file_path=res_file
+    )
+    minimizer.initialize(np.array([[20, 20], [20, 20], [15, 15]]))
+    minimizer.minimize(error_tolerance=1e-2)
+
