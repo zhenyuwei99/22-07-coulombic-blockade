454459ab7a33291aa613ceea6f8e5ba65243cd85
zhenyuwei99 <zhenyuwei99@gmail.com>
2022-08-30 09:54:31 +0800

    Update str parser

--- code/pnp_solver/str/parse.py
+++ code/pnp_solver/str/parse.py
@@ -10,12 +10,26 @@ copyright : (C)Copyright 2021-2021, Zhenyu Wei and Southeast University
 """
 
 import os
+import time
 import mdpy as md
 
-cur_dir = os.path.dirname(os.path.abspath(__file__))
-hdf5_file = os.path.join(cur_dir, "sio2_pore.hdf5")
-psf = md.io.PSFParser(os.path.join(cur_dir, "sio2_pore.psf"))
-pdb = md.io.PDBParser(os.path.join(cur_dir, "sio2_pore.pdb"))
+if __name__ == "__main__":
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    hdf5_file = os.path.join(cur_dir, "sio2_pore.hdf5")
+    if True:
+        s = time.time()
+        psf = md.io.PSFParser(os.path.join(cur_dir, "sio2_pore.psf"))
+        pdb = md.io.PDBParser(os.path.join(cur_dir, "sio2_pore.pdb"))
+        e = time.time()
+        print("Run xxx for %s s" % (e - s))
+        writer = md.io.HDF5Writer(
+            hdf5_file, topology=psf.topology, pbc_matrix=pdb.pbc_matrix
+        )
+        writer.write(pdb.positions)
 
-writer = md.io.HDF5Writer(hdf5_file, topology=psf.topology, pbc_matrix=pdb.pbc_matrix)
-writer.write(pdb.positions)
+    if True:
+        s = time.time()
+        hdf5 = md.io.HDF5Parser(hdf5_file)
+        e = time.time()
+        print("Run xxx for %s s" % (e - s))
+        print(hdf5.positions)
