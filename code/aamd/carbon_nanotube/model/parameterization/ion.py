1e32085f40871d0ce029965a724f4a24c93bacda
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-11-19 22:36:01 +0800

    Update parameterization code for ion

--- code/aamd/carbon_nanotube/model/parameterization/ion.py
+++ code/aamd/carbon_nanotube/model/parameterization/ion.py
@@ -19,40 +19,97 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from hydration import HydrationDistributionFunction
 
+ion, target = "cla", "oxygen"
+TRY = False
+NUM_LAYER = 4
+INITIAL_GUESS = [
+    [4, 3.1, 0.1],
+    [1, 3.7, 0.5],
+    [1.0, 5.2, 0.5],
+    [1.0, 6.3, 0.5],
+    [2, 8],
+]
+INITIAL_GUESS = [item for sublist in INITIAL_GUESS for item in sublist]
+print(INITIAL_GUESS)
 
-def predict(distance, h0, r0, sigma0, h1, r1, sigma1, rb, alpha):
-    g = HydrationDistributionFunction()
-    g.add_layer(height=h0, r0=r0, sigma=sigma0)
-    g.add_layer(height=h1, r0=r1, sigma=sigma1)
-    g.add_bulk(rb=rb, alpha=alpha)
+
+def get_g(params) -> HydrationDistributionFunction:
+    g = HydrationDistributionFunction(num_layers=NUM_LAYER)
+    for i in range(NUM_LAYER):
+        start = i * 3
+        g.add_layer(height=params[start], r0=params[start + 1], sigma=params[start + 2])
+    g.add_bulk(alpha=params[-2], rb=params[-1])
+    return g
+
+
+def predict(distance, params):
+    g = get_g(params)
     return g(distance=distance)
 
 
 def loss(params, args):
-    h0, r0, sigma0, h1, r1, sigma1, rb, alpha = params
     distance, ref = args
-    pred = predict(distance, h0, r0, sigma0, h1, r1, sigma1, rb, alpha)
-    error = (((pred - ref) ** 2) * np.abs(ref - 1)).mean()
-    error += r0 > r1
-    # error += r1 > rb
+    pred = predict(distance, params)
+    error = (((pred - ref)) ** 2).sum() * 0.5 / pred.shape[0]
+    # error = (((pred - ref)) ** 2 * (1 + ((ref - 1) * 2) ** 2)).mean() * 0.5
     return error
 
 
+def jac(params, args):
+    jac = []
+    distance, ref = args
+    pred = predict(distance, params)
+    factor = 1 / pred.shape[0]
+    delta = pred - ref
+    # Layer
+    for layer in range(NUM_LAYER):
+        start = layer * 3
+        h = params[start]
+        r = params[start + 1]
+        sigma = params[start + 2]
+        delta_d = distance - r
+        delta_d_square = delta_d**2
+        exp = np.exp(delta_d_square * (-0.5 / sigma**2))
+        exp = exp * delta
+        jac.append((exp).sum() * factor)  # h
+        exp = h * exp
+        jac.append((exp * delta_d).sum() * factor / sigma**2)  # r
+        jac.append((exp * delta_d_square).sum() * factor**2 / sigma**3)  # sigma
+    # Bulk
+    alpha = params[-2]
+    rb = params[-1]
+    delta_d = distance - rb
+    exp = np.exp(delta_d * (-alpha))
+    inv_denom = -delta / ((1 + exp) ** 2)
+    jac.append((-delta_d * exp * inv_denom).sum() * factor)  # alpha
+    jac.append((exp * inv_denom).sum() * (factor * -alpha))  # rb
+    print(jac)
+    return np.array(jac)
+
+
 def fit(distance, ref, out_file_path: str = None):
-    opt_res = optimize.dual_annealing(
+    bounds = []
+    for i in range(NUM_LAYER):
+        bounds += [(0.1, 10), (1, 8), (0.01, 10)]
+    bounds += [(1, 10), (2, 50)]
+    if TRY:
+        return INITIAL_GUESS
+    # opt_res = optimize.dual_annealing(
+    #     loss,
+    #     bounds=bounds,
+    #     args=([distance, ref],),
+    #     initial_temp=10000,
+    #     restart_temp_ratio=0.00001,
+    #     maxiter=5000,
+    #     callback=print,
+    # )
+    opt_res = optimize.minimize(
         loss,
-        bounds=[
-            (1, 10),
-            (1, 10),
-            (0.01, 2),
-            (1, 10),
-            (1, 10),
-            (0.01, 2),
-            (1, 10),
-            (1, 50),
-        ],
+        x0=INITIAL_GUESS,
         args=([distance, ref],),
-        # maxiter=2000,
+        method="BFGS",
+        jac="3-point",
+        hess="3-point",
         callback=print,
     )
     print(opt_res)
@@ -60,11 +117,7 @@ def fit(distance, ref, out_file_path: str = None):
 
 
 def save(json_file_path, params):
-    h0, r0, sigma0, h1, r1, sigma1, rb, alpha = params
-    g = HydrationDistributionFunction()
-    g.add_layer(height=h0, r0=r0, sigma=sigma0)
-    g.add_layer(height=h1, r0=r1, sigma=sigma1)
-    g.add_bulk(rb=rb, alpha=alpha)
+    g = get_g(params)
     g.save(json_file_path)
 
 
@@ -77,53 +130,45 @@ if __name__ == "__main__":
     img_file_path = os.path.join(
         os.path.join(out_dir, "image/parameterization_ion.png")
     )
-    ion, target = "pot", "oxygen"
     json_file_path = os.path.join(out_dir, "%s-%s.json" % (target, ion))
 
     data_dir = os.path.join(
-        cur_dir, "../../simulation/hydration_layer/out/no-wall-charge"
+        cur_dir, "../../simulation/hydration_layer/out/analysis_out"
     )
     result = md.analyser.load_analyser_result(
-        os.path.join(
-            data_dir,
-            "no-pore-w0-2.000A-ls-25.000A-POT-1.00e-01molPerL/%s-x-0.00A-y-0.00A-z-0.00A/%s-rdf-x-0.000A-y-0.000A-z-0.000A.npz"
-            % (ion, "O" if target == "oxygen" else "H"),
-        )
+        os.path.join(data_dir, "rdf-%s-%s.npz" % (ion, target))
     )
-    r = result.data["r_edge"][1:, 1:]
-    z = result.data["z_edge"][1:, 1:]
-    distance = np.sqrt(r**2 + z**2)
+    distance = result.data["bin_center"]
     ref = result.data["mean"]
     if True:
         params = fit(distance, ref)
+        save(json_file_path, params)
     else:
         g = HydrationDistributionFunction(json_file_path=json_file_path)
-        params = np.array(
-            [
-                g.layer_0_height,
-                g.layer_0_r0,
-                g.layer_0_sigma,
-                g.layer_1_height,
-                g.layer_1_r0,
-                g.layer_1_sigma,
-                g.bulk_alpha,
-                g.bulk_rb
-            ]
-        )
+        params = []
+        for i in range(g._num_layers):
+            layer_name = "layer_%d_" % i
+            params.append(getattr(g, layer_name + "height"))
+            params.append(getattr(g, layer_name + "r0"))
+            params.append(getattr(g, layer_name + "sigma"))
+        params += [g.bulk_alpha, g.bulk_rb]
+        params = np.array(params)
 
-    save(json_file_path, params)
-    pred = predict(distance, *params)
+    pred = predict(distance, params)
     fig, ax = plt.subplots(1, 2, figsize=[16, 9])
-    if not True:
-        all_res = np.stack([ref, pred])
-        norm = matplotlib.colors.Normalize(vmin=all_res.min(), vmax=all_res.max())
-        c = ax[0].contourf(r, z, ref, 200, norm=norm)
-        c = ax[1].contourf(r, z, pred, 200, norm=norm)
-        position = fig.add_axes([0.02, 0.10, 0.015, 0.80])  # 位置[左,下,右,上]
-        cb1 = fig.colorbar(c, cax=position)
-    else:
-        half_index = ref.shape[1] // 2
-        ax[0].plot(r[:, 1], ref[:, half_index])
-        ax[1].plot(r[:, 1], pred[:, half_index])
+    ax[0].plot(distance, ref)
+    ax[0].plot(distance, pred)
+    ax[0].set_yticks(np.linspace(ref.min(), ref.max(), 25, endpoint=True))
+
+    g = HydrationDistributionFunction(json_file_path=json_file_path)
+    for index, model in enumerate(g._model_list):
+        if index < NUM_LAYER:
+            start = index * 3
+            print(
+                "Layer %d height: %.3f ; r0: %.3f; sigma0: %.3f"
+                % (index, params[start], params[start + 1], params[start + 2])
+            )
+        ax[1].plot(distance, model(distance) + index, label="layer %d" % index)
+    fig.legend()
     plt.show()
-    fig.savefig(img_file_path)
+    # fig.savefig(img_file_path)
