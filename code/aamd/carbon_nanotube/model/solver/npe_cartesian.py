8d3fbc14f07df662ebbfc15fe9c60766eb146976
Zhenyu Wei <zhenyuwei99@gmail.com>
2023-01-10 18:29:19 +0800

    Replace NPESOlver with NPECartesianSolver

--- code/aamd/carbon_nanotube/model/solver/npe_cartesian.py
+++ code/aamd/carbon_nanotube/model/solver/npe_cartesian.py
@@ -25,7 +25,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from solver import *
 
 
-class NPESolver:
+class NPECartesianSolver:
     def __init__(self, grid: Grid, ion_type: str) -> None:
         """All grid and constant in default unit
         Variable:
