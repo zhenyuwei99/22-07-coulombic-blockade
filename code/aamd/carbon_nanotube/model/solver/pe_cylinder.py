b521b8b112de3319430bb40381916e52d65f0009
Zhenyu Wei <zhenyuwei99@gmail.com>
2023-01-20 20:22:58 +0800

    Update poisson equation solver in 2d cylinder coordinate system for trial

--- code/aamd/carbon_nanotube/model/solver/pe_cylinder.py
+++ code/aamd/carbon_nanotube/model/solver/pe_cylinder.py
@@ -23,7 +23,7 @@ from hydration import get_pore_distance
 
 
 class PECylinderSolver:
-    def __init__(self, grid) -> None:
+    def __init__(self, grid: Grid) -> None:
         """All grid and constant in default unit
         ### Coordinate:
         - r: radius
@@ -54,22 +54,42 @@ class PECylinderSolver:
 
     def _get_coefficient(self):
         pre_factor = NUMPY_FLOAT(0.5 / self._grid.grid_width**2)
+        half_width_factor = (
+            NUMBA_FLOAT(0.5 * self._grid.grid_width)
+            / self._grid.coordinate.r[1:-1, 1:-1]
+        )
         # 1: for plus and :-1 for minus
-        shape = [self._grid.num_dimensions] + [i + 1 for i in self._grid.inner_shape]
+        shape = [self._grid.num_dimensions, 2] + self._grid.inner_shape
         factor = cp.zeros(shape, CUPY_FLOAT)
         inv_denominator = cp.zeros(self._grid.inner_shape, CUPY_FLOAT)
         # r
-        factor[0, :, :-1] = pre_factor * (
-            self._grid.field.epsilon[1:, 1:-1] + self._grid.field.epsilon[:-1, 1:-1]
+        factor[0, 0] = (
+            pre_factor
+            * (CUPY_FLOAT(1) + half_width_factor)
+            * (
+                self._grid.field.epsilon[2:, 1:-1]
+                + self._grid.field.epsilon[1:-1, 1:-1]
+            )
         )
-        inv_denominator += factor[0, 1:, :-1] + factor[0, :-1, :-1]
+        factor[0, 1] = (
+            pre_factor
+            * (CUPY_FLOAT(1) - half_width_factor)
+            * (
+                self._grid.field.epsilon[:-2, 1:-1]
+                + self._grid.field.epsilon[1:-1, 1:-1]
+            )
+        )
+        inv_denominator += factor[0, 0] + factor[0, 1]
         # z
-        factor[1, :-1, :] = pre_factor * (
-            self._grid.field.epsilon[1:-1, 1:] + self._grid.field.epsilon[1:-1, :-1]
+        factor[1, 0] = pre_factor * (
+            self._grid.field.epsilon[2:, 1:-1] + self._grid.field.epsilon[1:-1, 1:-1]
         )
-        inv_denominator += factor[1, :-1, 1:] + factor[1, :-1, :-1]
+        factor[1, 0] = pre_factor * (
+            self._grid.field.epsilon[:-2, 1:-1] + self._grid.field.epsilon[1:-1, 1:-1]
+        )
+        inv_denominator += factor[1, 0] + factor[1, 1]
         inv_denominator = CUPY_FLOAT(1) / inv_denominator
-        return factor, inv_denominator
+        return factor.astype(CUPY_FLOAT), inv_denominator.astype(CUPY_FLOAT)
 
     def _update_boundary_point(self, phi):
         for boundary_type, boundary_data in phi.boundary.items():
@@ -97,11 +117,11 @@ class PECylinderSolver:
         factor_a, factor_b = CUPY_FLOAT(soa_factor), CUPY_FLOAT(1 - soa_factor)
         nominator = cp.zeros(self._grid.inner_shape, CUPY_FLOAT)
         # r
-        nominator += factor[0, 1:, :-1] * phi.value[2:, 1:-1]
-        nominator += factor[0, :-1, :-1] * phi.value[:-2, 1:-1]
+        nominator += factor[0, 0] * phi.value[2:, 1:-1]
+        nominator += factor[0, 1] * phi.value[:-2, 1:-1]
         # z
-        nominator += factor[1, :-1, 1:] * phi.value[1:-1, 2:]
-        nominator += factor[1, :-1, :-1] * phi.value[1:-1, :-2]
+        nominator += factor[1, 0] * phi.value[1:-1, 2:]
+        nominator += factor[1, 1] * phi.value[1:-1, :-2]
         # Add charge
         nominator += scaled_rho
         # Update
@@ -155,7 +175,9 @@ def get_phi(grid: Grid):
     if boundary_type == "neumann":
         boundary_data["self_index"] = boundary_self_index
         boundary_data["neighbor_index"] = boundary_neighbor_index
-        boundary_data["value"] = cp.zeros([boundary_self_index.shape[0]], CUPY_FLOAT)
+        boundary_data["value"] = (
+            cp.zeros([boundary_self_index.shape[0]], CUPY_FLOAT) + 0.001
+        )
     elif boundary_type == "dirichlet":
         boundary_data["index"] = boundary_self_index
         boundary_data["value"] = cp.zeros([boundary_self_index.shape[0]], CUPY_FLOAT)
@@ -190,14 +212,18 @@ if __name__ == "__main__":
     solver.iterate(10000)
 
     fig, ax = plt.subplots(1, 1, figsize=[16, 9])
-    phi = grid.variable.phi.value.get()
+    phi = grid.variable.phi.value.get()[1:-1, 1:-1]
     phi = Quantity(phi, default_energy_unit).convert_to(kilocalorie_permol).value
+    print(phi.max())
     threshold = 10
-    phi[phi >= threshold] = threshold
-    phi[phi <= -threshold] = -threshold
+    # phi[phi >= threshold] = threshold
+    # phi[phi <= -threshold] = -threshold
+    # facto, inv_denominator = solver._get_coefficient()
+    # phi = inv_denominator.get() == 0
+    # phi = grid.field.rho.get()[1:-1, 1:-1]
     c = ax.contourf(
-        grid.coordinate.r.get(),
-        grid.coordinate.z.get(),
+        grid.coordinate.r.get()[1:-1, 1:-1],
+        grid.coordinate.z.get()[1:-1, 1:-1],
         phi,
         200,
     )
