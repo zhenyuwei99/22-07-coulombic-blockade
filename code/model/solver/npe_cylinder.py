6460ed1cef942c5cf177a2d6908d65fd5ffe8b72
Zhenyu Wei <zhenyuwei99@gmail.com>
2023-02-18 20:16:20 +0800

    Update model.solver.NPECylinderSolver, r-no-flux with second order

--- code/model/solver/npe_cylinder.py
+++ code/model/solver/npe_cylinder.py
@@ -19,7 +19,7 @@ from model.core import Grid
 
 
 class NPECylinderSolver:
-    def __init__(self, grid: Grid, ion_type: str, is_inverse=False) -> None:
+    def __init__(self, grid: Grid, ion_type: str) -> None:
         """All grid and constant in default unit
         ### Coordinate:
         - r: radius
@@ -52,7 +52,6 @@ class NPECylinderSolver:
         if not ion_type in ION_DICT.keys():
             raise KeyError("Ion %s is not supported" % ion_type)
         self._ion_type = ion_type
-        self._is_inverse = is_inverse
         # Requirement
         self._grid.add_requirement("variable", "rho_%s" % self._ion_type)
         self._grid.add_requirement("field", "u_%s" % self._ion_type)
@@ -71,9 +70,11 @@ class NPECylinderSolver:
             "r-no-flux": self._get_r_no_flux_points,
             "z-no-flux": self._get_z_no_flux_points,
             "no-flux-inner": self._get_no_flux_inner_points,
-            "z-no-gradient": self._get_z_no_gradient_points,
-            "stern": self._get_stern_points,
         }
+        # Attribute
+        # self._u_h2
+        # self._delta_u
+        # self._upwind
 
     def _get_equation(self, rho):
         data, row, col = [], [], []
@@ -99,6 +100,9 @@ class NPECylinderSolver:
         )
         # Return
         return matrix.tocsr(), vector.astype(CUPY_FLOAT)
+
+    def _update_factor(self):
+        pass
 
     def _get_inner_points(self, scaled_u, index):
         data, row, col = [], [], []
@@ -176,55 +180,6 @@ class NPECylinderSolver:
             vector.astype(CUPY_FLOAT),
         )
 
-    def _get_no_flux_inner_points(self, scaled_u, index, unit_vec):
-        data, row, col = [], [], []
-        z_shape = CUPY_INT(self._grid.shape[1])
-        row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
-        direction = unit_vec.copy()
-        direction[direction >= 0] = 1
-        direction[direction < 0] = -1
-        direction = direction.astype(CUPY_INT)
-        self_index = (index[:, 0], index[:, 1])
-        factor = cp.zeros_like(direction, CUPY_INT)
-        factor[:, 0] = (
-            scaled_u[index[:, 0] + direction[:, 0], index[:, 1]] <= scaled_u[self_index]
-        ).astype(CUPY_INT)
-        factor[:, 1] = (
-            scaled_u[index[:, 0], index[:, 1] + direction[:, 1]] <= scaled_u[self_index]
-        ).astype(CUPY_INT)
-        factor[direction == 0] = CUPY_INT(-1)
-        direction *= factor
-        factor = direction.astype(CUPY_FLOAT)
-        factor *= unit_vec
-        r_plus = (index[:, 0] + direction[:, 0], index[:, 1])
-        z_plus = (index[:, 0], index[:, 1] + direction[:, 1])
-        self_index = (index[:, 0], index[:, 1])
-        inv_h2 = CUPY_FLOAT(1 / self._grid.grid_width**2)
-        inv_h2 += cp.zeros(index.shape[0], CUPY_FLOAT)
-        for i in range(3):
-            row.append(row_index)
-        # r plus
-        offset = z_shape * direction[:, 0]
-        data.append(inv_h2 * factor[:, 0])
-        col.append(row_index + offset)
-        # z plus
-        data.append(inv_h2 * factor[:, 1])
-        col.append(row_index + direction[:, 1])
-        # self
-        self_factor = factor[:, 0] * (scaled_u[r_plus] - scaled_u[self_index] - inv_h2)
-        self_factor += factor[:, 1] * (scaled_u[z_plus] - scaled_u[self_index] - inv_h2)
-        data.append(self_factor)
-        col.append(row_index)
-        # Vector
-        vector = cp.zeros(self._grid.num_points, CUPY_FLOAT)
-        # Return
-        return (
-            cp.hstack(data).astype(CUPY_FLOAT),
-            cp.hstack(row).astype(CUPY_INT),
-            cp.hstack(col).astype(CUPY_INT),
-            vector.astype(CUPY_FLOAT),
-        )
-
     def _get_dirichlet_points(self, scaled_u, index, value):
         z_shape = CUPY_INT(self._grid.shape[1])
         row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
@@ -236,100 +191,6 @@ class NPECylinderSolver:
             cp.hstack(data).astype(CUPY_FLOAT),
             cp.hstack(row_index).astype(CUPY_INT),
             cp.hstack(row_index).astype(CUPY_INT),
-            vector.astype(CUPY_FLOAT),
-        )
-
-    def _get_stern_points(self, scaled_u, index, density):
-        h2 = CUPY_FLOAT(self._grid.grid_width**2)
-        z_shape = CUPY_INT(self._grid.shape[1])
-        row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
-        data = cp.ones_like(row_index, CUPY_FLOAT)
-        # Vector
-        vector = cp.zeros(self._grid.num_points, CUPY_FLOAT)
-        vector[row_index] = density * cp.exp(-scaled_u[index[:, 0], index[:, 1]] * h2)
-        # Return
-        return (
-            data.astype(CUPY_FLOAT),
-            row_index.astype(CUPY_INT),
-            row_index.astype(CUPY_INT),
-            vector.astype(CUPY_FLOAT),
-        )
-
-    def _get_r_no_flux_points(self, scaled_u, index, direction):
-        data, row, col = [], [], []
-        z_shape = CUPY_INT(self._grid.shape[1])
-        row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
-        self_index = (index[:, 0], index[:, 1])
-        r_plus_1 = (index[:, 0] + direction, index[:, 1])
-        inv_h2 = CUPY_FLOAT(1 / self._grid.grid_width**2)
-        inv_h2 += cp.zeros(index.shape[0], CUPY_FLOAT)
-        for i in range(2):
-            row.append(row_index)
-        # r+1
-        offset = z_shape * direction
-        data.append(inv_h2)
-        col.append(row_index + offset)
-        # self
-        data.append(scaled_u[r_plus_1] - scaled_u[self_index] - inv_h2)
-        col.append(row_index)
-        # Vector
-        vector = cp.zeros(self._grid.num_points, CUPY_FLOAT)
-        # Return
-        return (
-            cp.hstack(data).astype(CUPY_FLOAT),
-            cp.hstack(row).astype(CUPY_INT),
-            cp.hstack(col).astype(CUPY_INT),
-            vector.astype(CUPY_FLOAT),
-        )
-
-    def _get_z_no_flux_points(self, scaled_u, index, direction):
-        data, row, col = [], [], []
-        z_shape = CUPY_INT(self._grid.shape[1])
-        row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
-        self_index = (index[:, 0], index[:, 1])
-        z_plus_1 = (index[:, 0], index[:, 1] + direction)
-        inv_h2 = CUPY_FLOAT(1 / self._grid.grid_width**2)
-        inv_h2 += cp.zeros(index.shape[0], CUPY_FLOAT)
-        for i in range(2):
-            row.append(row_index)
-        # z+1
-        offset = direction
-        data.append(inv_h2)
-        col.append(row_index + offset)
-        # self
-        data.append(scaled_u[z_plus_1] - scaled_u[self_index] - inv_h2)
-        col.append(row_index)
-        # Vector
-        vector = cp.zeros(self._grid.num_points, CUPY_FLOAT)
-        # Return
-        return (
-            cp.hstack(data).astype(CUPY_FLOAT),
-            cp.hstack(row).astype(CUPY_INT),
-            cp.hstack(col).astype(CUPY_INT),
-            vector.astype(CUPY_FLOAT),
-        )
-
-    def _get_z_no_gradient_points(self, scaled_u, index, direction):
-        data, row, col = [], [], []
-        z_shape = CUPY_INT(self._grid.shape[1])
-        row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
-        for i in range(2):
-            row.append(row_index)
-        factor = cp.ones(index.shape[0], CUPY_FLOAT)
-        # Neighbor
-        offset = direction
-        data.append(factor)
-        col.append(row_index + offset)
-        # self
-        data.append(-factor)
-        col.append(row_index)
-        # Vector
-        vector = cp.zeros(self._grid.num_points, CUPY_FLOAT)
-        # Return
-        return (
-            cp.hstack(data).astype(CUPY_FLOAT),
-            cp.hstack(row).astype(CUPY_INT),
-            cp.hstack(col).astype(CUPY_INT),
             vector.astype(CUPY_FLOAT),
         )
 
@@ -384,7 +245,120 @@ class NPECylinderSolver:
             vector.astype(CUPY_FLOAT),
         )
 
-    def iterate(self, num_iterations, is_restart=False, solver_freq=50):
+    def _get_no_flux_inner_points(self, scaled_u, index, unit_vec):
+        data, row, col = [], [], []
+        z_shape = CUPY_INT(self._grid.shape[1])
+        row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
+        direction = unit_vec.copy()
+        direction[direction >= 0] = 1
+        direction[direction < 0] = -1
+        direction = direction.astype(CUPY_INT)
+        self_index = (index[:, 0], index[:, 1])
+        factor = cp.zeros_like(direction, CUPY_INT)
+        factor[:, 0] = (
+            scaled_u[index[:, 0] + direction[:, 0], index[:, 1]] <= scaled_u[self_index]
+        ).astype(CUPY_INT)
+        factor[:, 1] = (
+            scaled_u[index[:, 0], index[:, 1] + direction[:, 1]] <= scaled_u[self_index]
+        ).astype(CUPY_INT)
+        factor[direction == 0] = CUPY_INT(-1)
+        direction *= factor
+        factor = direction.astype(CUPY_FLOAT)
+        factor *= unit_vec
+        r_plus = (index[:, 0] + direction[:, 0], index[:, 1])
+        z_plus = (index[:, 0], index[:, 1] + direction[:, 1])
+        self_index = (index[:, 0], index[:, 1])
+        inv_h2 = CUPY_FLOAT(1 / self._grid.grid_width**2)
+        inv_h2 += cp.zeros(index.shape[0], CUPY_FLOAT)
+        for i in range(3):
+            row.append(row_index)
+        # r plus
+        offset = z_shape * direction[:, 0]
+        data.append(inv_h2 * factor[:, 0])
+        col.append(row_index + offset)
+        # z plus
+        data.append(inv_h2 * factor[:, 1])
+        col.append(row_index + direction[:, 1])
+        # self
+        self_factor = factor[:, 0] * (scaled_u[r_plus] - scaled_u[self_index] - inv_h2)
+        self_factor += factor[:, 1] * (scaled_u[z_plus] - scaled_u[self_index] - inv_h2)
+        data.append(self_factor)
+        col.append(row_index)
+        # Vector
+        vector = cp.zeros(self._grid.num_points, CUPY_FLOAT)
+        # Return
+        return (
+            cp.hstack(data).astype(CUPY_FLOAT),
+            cp.hstack(row).astype(CUPY_INT),
+            cp.hstack(col).astype(CUPY_INT),
+            vector.astype(CUPY_FLOAT),
+        )
+
+    def _get_r_no_flux_points(self, scaled_u, index, direction):
+        data, row, col = [], [], []
+        z_shape = CUPY_INT(self._grid.shape[1])
+        row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
+        self_index = (index[:, 0], index[:, 1])
+        r_plus_1 = (index[:, 0] + direction, index[:, 1])
+        r_plus_2 = (index[:, 0] + direction + direction, index[:, 1])
+        inv_h2 = CUPY_FLOAT(1 / self._grid.grid_width**2)
+        inv_h2 += cp.zeros(index.shape[0], CUPY_FLOAT)
+        for i in range(3):
+            row.append(row_index)
+        # r+1
+        offset = z_shape * direction
+        data.append(inv_h2 * CUPY_FLOAT(4))
+        col.append(row_index + offset)
+        # r+1
+        offset = z_shape * direction
+        data.append(inv_h2 * CUPY_FLOAT(-1))
+        col.append(row_index + offset)
+        # self
+        data.append(
+            scaled_u[r_plus_1] * CUPY_FLOAT(4)
+            - scaled_u[r_plus_2]
+            - scaled_u[self_index] * CUPY_FLOAT(3)
+            - inv_h2 * CUPY_FLOAT(3)
+        )
+        col.append(row_index)
+        # Vector
+        vector = cp.zeros(self._grid.num_points, CUPY_FLOAT)
+        # Return
+        return (
+            cp.hstack(data).astype(CUPY_FLOAT),
+            cp.hstack(row).astype(CUPY_INT),
+            cp.hstack(col).astype(CUPY_INT),
+            vector.astype(CUPY_FLOAT),
+        )
+
+    def _get_z_no_flux_points(self, scaled_u, index, direction):
+        data, row, col = [], [], []
+        z_shape = CUPY_INT(self._grid.shape[1])
+        row_index = (index[:, 0] * z_shape + index[:, 1]).astype(CUPY_INT)
+        self_index = (index[:, 0], index[:, 1])
+        z_plus_1 = (index[:, 0], index[:, 1] + direction)
+        inv_h2 = CUPY_FLOAT(1 / self._grid.grid_width**2)
+        inv_h2 += cp.zeros(index.shape[0], CUPY_FLOAT)
+        for i in range(2):
+            row.append(row_index)
+        # z+1
+        offset = direction
+        data.append(inv_h2)
+        col.append(row_index + offset)
+        # self
+        data.append(scaled_u[z_plus_1] - scaled_u[self_index] - inv_h2)
+        col.append(row_index)
+        # Vector
+        vector = cp.zeros(self._grid.num_points, CUPY_FLOAT)
+        # Return
+        return (
+            cp.hstack(data).astype(CUPY_FLOAT),
+            cp.hstack(row).astype(CUPY_INT),
+            cp.hstack(col).astype(CUPY_INT),
+            vector.astype(CUPY_FLOAT),
+        )
+
+    def iterate(self, num_iterations, is_restart=False, solver_freq=100):
         self._grid.check_requirement()
         rho = getattr(self._grid.variable, "rho_%s" % self._ion_type)
         self._matrix, self._vector = self._get_equation(rho)
@@ -432,142 +406,3 @@ class NPECylinderSolver:
         res = rho.reshape(self._grid.num_points)
         residual = (cp.abs(self._matrix.dot(res) - self._vector)).mean()
         return residual
-
-    @property
-    def is_inverse(self):
-        return self._is_inverse
-
-    @is_inverse.setter
-    def is_inverse(self, is_inverse: bool):
-        self._is_inverse = is_inverse
-
-
-def get_rho(grid: Grid):
-    density = Quantity(0.15, mol / decimeter**3) * NA
-    density = check_quantity_value(density, 1 / default_length_unit**3)
-    rho = grid.empty_variable()
-    field = grid.zeros_field().astype(CUPY_INT) - 1
-    value = grid.zeros_field().astype(CUPY_FLOAT)
-    direction = grid.zeros_field().astype(CUPY_INT)
-    r_direction = grid.zeros_field().astype(CUPY_INT)
-    z_direction = grid.zeros_field().astype(CUPY_INT)
-    # 0: inner; 1: dirichlet; 2: no-gradient; 3: axial-symmetry
-    # Inner
-    field[1:-1, 1:-1] = 0
-    # dirichlet
-    field[:, [0, -1]] = 1
-    value[:, 0] = density
-    value[:, -1] = 0
-    # z-no-flux
-    r_min_index = 100
-    min_index, max_index = 100, 400
-    field[r_min_index:-1, min_index] = 2
-    direction[r_min_index:-1, min_index] = 1
-    field[r_min_index:-1, max_index] = 2
-    direction[r_min_index:-1, max_index] = -1
-    # r-no-flux
-    field[r_min_index, min_index:max_index] = 3
-    direction[r_min_index, min_index:max_index] = 1
-    field[-1, 1:-1] = 3
-    direction[-1, 1:-1] = -1
-    # no-flux
-    field[r_min_index, [min_index, max_index]] = 3
-    direction[r_min_index, [min_index, max_index]] = 3
-    r_direction[r_min_index, [min_index, max_index]] = 1
-    z_direction[r_min_index, min_index] = 1
-    z_direction[r_min_index, max_index] = -1
-    # axial-symmetry
-    field[0, 1:-1] = 4
-
-    field[r_min_index + 1 :, min_index + 1 : max_index] = 1
-    value[r_min_index + 1 :, min_index + 1 : max_index] = 0
-    # Register
-    index = cp.argwhere(field == 0).astype(CUPY_INT)
-    rho.register_points(
-        type="inner",
-        index=index,
-    )
-    index = cp.argwhere(field == 1).astype(CUPY_INT)
-    rho.register_points(
-        type="dirichlet",
-        index=index,
-        value=value[index[:, 0], index[:, 1]],
-    )
-    index = cp.argwhere(field == 2).astype(CUPY_INT)
-    rho.register_points(
-        type="z-no-flux",
-        index=index,
-        direction=direction[index[:, 0], index[:, 1]],
-    )
-    index = cp.argwhere(field == 3).astype(CUPY_INT)
-    rho.register_points(
-        type="r-no-flux",
-        index=index,
-        direction=direction[index[:, 0], index[:, 1]],
-    )
-    index = cp.argwhere(field == 4).astype(CUPY_INT)
-    rho.register_points(
-        type="no-flux",
-        index=index,
-        r_direction=r_direction[index[:, 0], index[:, 1]],
-        z_direction=z_direction[index[:, 0], index[:, 1]],
-    )
-    index = cp.argwhere(field == 5).astype(CUPY_INT)
-    rho.register_points(type="axial-symmetry", index=index)
-    return rho
-
-
-def get_u(grid: Grid, r0=10, z0=5):
-    sigma = grid.grid_width * 10
-    height = Quantity(0.5, kilocalorie_permol).convert_to(default_energy_unit).value
-    dist = (grid.coordinate.r - r0) ** 2 + (grid.coordinate.z - z0) ** 2
-    u = cp.exp(-dist / (2 * sigma**2)) * height
-
-    # dist = (grid.coordinate.r - r0 - 10) ** 2 + (grid.coordinate.z - z0) ** 2
-    # u += cp.exp(-dist / (2 * sigma**2)) * -height
-    u = grid.zeros_field(CUPY_FLOAT)
-    return u.astype(CUPY_FLOAT)
-
-
-if __name__ == "__main__":
-    import time
-    import matplotlib.pyplot as plt
-
-    beta = (Quantity(300, kelvin) * KB).convert_to(default_energy_unit).value
-    beta = 1 / beta
-
-    grid = Grid(grid_width=0.25, r=[0, 50], z=[-100, 100])
-    solver = NPECylinderSolver(grid=grid, ion_type="k")
-    grid.add_variable("rho_k", get_rho(grid))
-    grid.add_field("u_k", get_u(grid))
-    grid.add_constant("beta", beta)
-
-    s = time.time()
-    solver.iterate(5000)
-    print(solver.residual)
-    e = time.time()
-    print("Run xxx for %s s" % (e - s))
-
-    s = time.time()
-    solver.iterate(5000, False)
-    e = time.time()
-    print("Run xxx for %s s" % (e - s))
-
-    rho = grid.variable.rho_k.value.get()
-    rho = (
-        (Quantity(rho, 1 / default_length_unit**3) / NA)
-        .convert_to(mol / decimeter**3)
-        .value
-    )
-
-    # flux = solver.get_flux(0).get()
-    fig, ax = plt.subplots(1, 1, figsize=[16, 9])
-    threshold = 200
-    c = ax.contour(
-        grid.coordinate.r.get(),
-        grid.coordinate.z.get(),
-        rho,
-        50,
-    )
-    fig.colorbar(c)
-    plt.show()
